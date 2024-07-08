1bf79d5902a494b9814bbb9ac3c585b6443e7a3f
diegofernandes.47 <diegof@47.github.com>
2024-07-08 11:18:59 -0300

    Update ex006.c

--- exercícios/ex006.c
+++ exercícios/ex006.c
@@ -4,7 +4,7 @@ int main() {
 
 int batidas_ano;
 printf("Digite a sua idade para saber quantos batimentos voc� ja teve: ");
-scanf("%d", &bat_ano);
+scanf("%d", &batidas_ano);
 printf("A quantidade de batimentos vai ser " "%d", batidas_ano * 365 * 24 * 60 * 60); 
 
 return 0;
